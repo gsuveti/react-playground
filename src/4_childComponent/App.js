34c35a8181674d37239f96fbbd521c926ba2a996
georgesuveti <george.suveti@irian.ro>
2017-10-21 16:46:12 +0300

    child component demo

---
+++ src/4_childComponent/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Post from "./Post";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className={"row"}>
+                    <h1 className="col-xs-12 text-center">Welcome to my blog!</h1>
+                    <Post></Post>
+                    <Post></Post>
+                    <Post></Post>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 
 // import App from './1_helloWorld/App';
 // import App from './2_component/App';
-import App from './3_bootstrap/App';
+// import App from './3_bootstrap/App';
+import App from './4_childComponent/App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
