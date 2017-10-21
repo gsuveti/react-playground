03d18c3b803ca79d6da3e3969895fe9f62b04bf7
georgesuveti <george.suveti@irian.ro>
2017-10-21 16:28:43 +0300

    hello world, component & bootstrap

---
+++ src/3_bootstrap/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className={"row"}>
+                    <h1 className="col-xs-12 text-center bg-success">Hello from component!</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+// import App from './1_helloWorld/App';
+// import App from './2_component/App';
+import App from './3_bootstrap/App';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
