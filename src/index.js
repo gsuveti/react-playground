af701e656a2690253265da65b016db45ecd61a7d
georgesuveti <george.suveti@irian.ro>
2017-10-23 11:23:12 +0300

    props demo

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-
 // import App from './1_helloWorld/App';
 // import App from './2_component/App';
 // import App from './3_bootstrap/App';
-import App from './4_childComponent/App';
+// import App from './4_childComponent/App';
+import App from './5_props/App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
